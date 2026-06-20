794d469ce479be40b8c2cfbfd173b6208801ad8a
emaldev <261611436+emaldev@users.noreply.github.com>
2026-06-20 21:07:56 +0430

    java HashMap and Map

--- HelloWorde.java
+++ HelloWorde.java
@@ -2,22 +2,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HelloWorde{
-    public static void main(String[] args){
+    public  static void main(String[] args){
         HashMap<String, Integer> name = new HashMap<>();
 
-        name.put("Loptope", 12);
-        name.put("Mouse", 3);
-        name.put("HardDisk", 90);
+        name.put("Loptop", 12);
+        name.put("Apple", 14);
+        name.put("coffee", 23);
 
-        System.out.println(name.get("Mouse"));
-     
-        int seal = name.get("Mouse");
-        name.put("Mouse", seal-1);
+        int seal = name.get("Apple");
+        name.put("Apple", seal-1);
 
-        System.out.println("this is the for-each loop");
-        for(Map.Entry<String, Integer> entry: name.entrySet() ){
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+        for(Map.Entry<String, Integer> entry: name.entrySet()){
+        System.out.println("This is " + entry.getKey() + ": " + entry.getValue());
         }
-
+        
     }
 }
