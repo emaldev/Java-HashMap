e84fd2405bb4adbdf629fbb20236bd6d80d6deba
emaldev <261611436+emaldev@users.noreply.github.com>
2026-06-21 01:21:03 +0430

    add dynamic student grade manager with scanner input

--- StudentGradeManager.java
+++ StudentGradeManager.java
@@ -1,33 +1,40 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 public class StudentGradeManager {
     public static void main(String[] args){
-        HashMap<String, Integer> studentgrade  = new HashMap<>();
-
-        studentgrade.put("Ahmad", 18);
-        studentgrade.put("Ali", 8);
-        studentgrade.put("Mohammad", 18);
-        studentgrade.put("Sara", 19);
-        studentgrade.put("Mehnaz", 16);
-        studentgrade.put("Mohsen", 20);
+        HashMap<String, Double> studentgrade  = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        double grade;
+        String  name;
+        
+         
+             for(int i = 0; i < 10; i++){
+                System.out.println("Enter name and grade student"+ (i + 1 ) + ": ");
+                name = input.nextLine();
+                grade =  input.nextDouble();
+                input.nextLine();
+                
+                studentgrade.put(name, grade);
+             }
 
         System.out.println("---------------------this is the student grade.------------------------------");
 
-        int sel = studentgrade.get("Mohammad");
-        studentgrade.put("Mohammad", sel -3);
 
-         
-        for(Map.Entry<String, Integer> entry: studentgrade.entrySet()){
+
+
+        for(Map.Entry<String, Double> entry: studentgrade.entrySet()){
              System.out.println("====================<<<<<<<<<<<<<<<<<<<:>>>>>>>>>>>>>>>>>======================");
-            if(entry.getValue() < 10){
-                System.out.println("this is a  Lazy student. " + entry.getKey() + ":> " + entry.getValue());
-
-            }else if (entry.getValue() >= 18){
-                System.out.println("This is a Average Student." + entry.getKey() + ":> " + entry.getValue());
-
-            }else if (entry.getValue() >= 20){
+             if (entry.getValue() >= 20){
 
                 System.out.println("This is a Capable Student. I love it." + entry.getKey()+ ":> " + entry.getValue());
+              }else if (entry.getValue() >= 18){
+                System.out.println("This is a Average Student." + entry.getKey() + ":> " + entry.getValue());
+
+            }else if(entry.getValue() < 10 ){
+                System.out.println("this is a  Lazy student. " + entry.getKey() + ":> " + entry.getValue());
+
+            
             }else if(entry.getValue() >= 10 && entry.getValue() <=16){
                 System.out.println("this is good Student." + entry.getKey()+":> " + entry.getValue());
             }
